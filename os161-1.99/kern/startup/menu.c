bba88ca47038a9e49a5372057cacb82f63eed922
pn2wong <pn2wong@student.cs.uwaterloo.ca>
2014-05-14 11:54:49 -0400

    A0 actually complete

--- os161-1.99/kern/startup/menu.c
+++ os161-1.99/kern/startup/menu.c
@@ -289,6 +289,13 @@ cmd_quit(int nargs, char **args)
 	return 0;
 }
 
+static int cmd_dth(int nargs, char **args)
+{
+	(void)nargs;
+	(void)args;
+	dbflags = 16;
+	return 0;
+}
 /*
  * Command for mounting a filesystem.
  */
@@ -437,6 +444,7 @@ static const char *opsmenu[] = {
 	"[sync]    Sync filesystems          ",
 	"[panic]   Intentional panic         ",
 	"[q]       Quit and shut down        ",
+	"[dth] 	  Enable DB_THREADS debugging",
 	NULL
 };
 
@@ -548,6 +556,7 @@ static struct {
 	{ "q",		cmd_quit },
 	{ "exit",	cmd_quit },
 	{ "halt",	cmd_quit },
+	{ "dth",	cmd_dth },
 
 #if OPT_SYNCHPROBS
 	/* in-kernel synchronization problem(s) */
